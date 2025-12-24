febe987ca91a1a3da0964de4ed66d5e05fd688f4
Kanishk Ujjwal <kanishkujjwal1176@gmail.com>
2025-12-24 22:56:42 +0530

    Add functionality to extract and reverse list elements

---
+++ Assignment5_task2.py
@@ -0,0 +1,6 @@
+original_list = [i for i in range (1,11)]
+print (f"Original list :{original_list}")
+l1 = original_list[0:5]
+l2 = original_list[4::-1]
+print (f"Extracted first five elements :{l1}")
+print (f"Reverse extracted elements :{l2}")
